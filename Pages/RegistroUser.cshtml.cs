4826b3e59aece0d89f550abdbaf0bdc43e442202
Luis Delzo E <ldelzo.e@gmail.com>
2020-04-15 19:56:16 -0500

    Login.cshtml

---
+++ Pages/RegistroUser.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Batman_01.Pages
+{
+    public class RegistroUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
